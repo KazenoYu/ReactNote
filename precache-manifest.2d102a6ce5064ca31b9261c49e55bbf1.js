d4716df7d154ee333827691d4a953425d0b05549
Eating <f.t.aisamc@gmail.com>
2022-09-21 09:08:11 +0800

    Updates

---
+++ precache-manifest.2d102a6ce5064ca31b9261c49e55bbf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b4359f6eac5433c5a48",
+    "url": "/ReactNote/static/css/main.3aa2bfaf.chunk.css"
+  },
+  {
+    "revision": "2b4359f6eac5433c5a48",
+    "url": "/ReactNote/static/js/main.2b4359f6.chunk.js"
+  },
+  {
+    "revision": "7949d88931629ff8d42c",
+    "url": "/ReactNote/static/js/1.7949d889.chunk.js"
+  },
+  {
+    "revision": "b5664b33a5631b20721b",
+    "url": "/ReactNote/static/js/runtime~main.b5664b33.js"
+  },
+  {
+    "revision": "48907a2ce93b04d63207c2aa70d15479",
+    "url": "/ReactNote/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ReactNote/precache-manifest.2d102a6ce5064ca31b9261c49e55bbf1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ReactNote/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
